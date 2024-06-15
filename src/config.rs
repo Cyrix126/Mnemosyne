0dc4bf4b3c71c9b9c249a8254d33e0e3223b9aaf
Louis-Marie Baer <lm@baermail.fr>
2024-06-15 09:48:59 +0200

    feat: set reasonnable default configuration for cache

--- src/config.rs
+++ src/config.rs
@@ -53,10 +53,21 @@ impl Config {
     }
 }
 
-#[derive(Serialize, Deserialize, Default, Clone)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct CacheConfig {
     /// cache expiration after last request
     pub expiration: Duration,
     /// in megabytes, the maximum size of memory the cache can take.
     pub size_limit: u64,
+}
+
+/// About a month to clear unused entries (if there still room)
+/// 250Mb of memory space for the cache
+impl Default for CacheConfig {
+    fn default() -> Self {
+        Self {
+            expiration: Duration::from_secs(2592000),
+            size_limit: 250,
+        }
+    }
 }
