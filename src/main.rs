cdf63b2f1a886dfe8007daab9a3161064334a098
Louis-Marie Baer <lm@baermail.fr>
2024-07-08 13:17:43 +0200

    feat: add first request test

--- src/main.rs
+++ src/main.rs
@@ -34,21 +34,78 @@ async fn main() -> Result<()> {
     let config = confy::load_path::<Config>("/etc/mnemosyne/config.toml")?;
     let listen = config.listen_address;
     info!("creating the cache and index...");
-    let state = AppState {
-        cache: Cache::new(&config),
-        config,
-        index_cache: Arc::new(Mutex::new(IndexCache::new())),
-        client: Client::new(),
-    };
+    let state = new_state(config);
     info!("Done.");
     // create route for cache API
-    let route = Router::new()
-        .route("/delete/:uuid", delete(api::delete_entry))
-        .route("/delete_all", delete(api::delete_all))
-        .fallback(api::handler)
-        .with_state(state);
+    let route = router().with_state(state);
     info!("starting to listen on {listen}");
     let listener = tokio::net::TcpListener::bind(listen).await?;
     axum::serve(listener, route.into_make_service()).await?;
     Ok(())
+}
+
+fn router() -> Router<AppState> {
+    Router::new()
+        .route("/delete/:uuid", delete(api::delete_entry))
+        .route("/delete_all", delete(api::delete_all))
+        .fallback(api::handler)
+}
+fn new_state(config: Config) -> AppState {
+    AppState {
+        cache: Cache::new(&config),
+        config,
+        index_cache: Arc::new(Mutex::new(IndexCache::new())),
+        client: Client::new(),
+    }
+}
+// tests
+
+#[cfg(test)]
+// backend
+mod test {
+    use anyhow::Result;
+    use axum::{routing::get, Router};
+    use axum_test::TestServer;
+    use tokio::{net::TcpListener, spawn};
+    use url::Url;
+
+    use crate::{config::Config, new_state, router};
+
+    async fn backend_handler() -> &'static str {
+        "Hello, World!"
+    }
+    fn router_backend() -> Router {
+        Router::new().route("/", get(backend_handler))
+    }
+    // needs to start a backend service, will be assigned an open port by the os
+    async fn app_backend(listener: TcpListener) -> Result<()> {
+        axum::serve(listener, router_backend().into_make_service()).await?;
+        Ok(())
+    }
+    #[tokio::test]
+    async fn first_request() -> Result<()> {
+        tracing_subscriber::fmt::init();
+        // start backend service
+        let listener = tokio::net::TcpListener::bind("127.0.0.1:0").await?;
+        let port = listener.local_addr().unwrap().port();
+        spawn(async move { app_backend(listener).await });
+        // configuration of Mnemosyne
+        let config = Config {
+            endpoints: vec![(
+                "/test".to_string(),
+                Url::parse(&format!("http://127.0.0.1:{port}"))?,
+            )],
+            ..Default::default()
+        };
+        // state of Mnemosyne
+        let state = new_state(config);
+        // router
+        let router = router().with_state(state);
+        // start Mnemosyne
+        let app = TestServer::new(router).unwrap();
+        // send get request for the first time
+        let rep = app.get("/test").await;
+        rep.assert_status_ok();
+        Ok(())
+    }
 }
